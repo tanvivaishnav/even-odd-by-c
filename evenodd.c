e7b16964040c12a6e05791496ad438fd0db07d73
sakshijain1 <asakshi1905@gmail.com>
2018-01-12 19:51:39 +0530

    first commit

---
+++ evenodd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int no;
+printf("\n\t Enter a no. : ");
+scanf("%d",&no);
+
+if(no%2==0)
+{
+printf("\n\t Number is even ");
+}
+else
+{
+printf("\n\t Number is odd ");
+}
+}
